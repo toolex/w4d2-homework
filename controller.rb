2abd79b37f2b5b87a64de7dea463482e49ebd57d
Iain Tooley <iaintooley1@gmail.com>
2019-10-01 16:12:50 +0100

    added css

--- controller.rb
+++ controller.rb
@@ -4,6 +4,21 @@ require("sinatra/contrib/all") if development?
 require_relative("./models/rock_paper_scissors")
 also_reload("./models/*")
 
+get "/" do
+  erb(:home)
+end
+
+get "/rock" do
+  erb(:rock)
+end
+
+get "/paper" do
+  erb(:paper)
+end
+
+get "/scissors" do
+  erb(:scissors)
+end
 
 get "/rock/:fight" do
   @result = Rps.rock(params[:fight])
