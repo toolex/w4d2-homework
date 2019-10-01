7e9bf8e9b8a27cbf605d8114380cd58bb92c14dd
Iain Tooley <iaintooley1@gmail.com>
2019-10-01 15:53:37 +0100

    mvp complete

--- controller.rb
+++ controller.rb
@@ -1,2 +1,21 @@
 require("sinatra")
 require("sinatra/contrib/all") if development?
+
+require_relative("./models/rock_paper_scissors")
+also_reload("./models/*")
+
+
+get "/rock/:fight" do
+  @result = Rps.rock(params[:fight])
+  erb(:result)
+end
+
+get "/paper/:fight" do
+  @result = Rps.paper(params[:fight])
+  erb(:result)
+end
+
+get "/scissors/:fight" do
+  @result = Rps.scissors(params[:fight])
+  erb(:result)
+end
